f83bf013b3829857ecbfd3a8d7f11f83834ef2cb
coding-frog117 <syeshe2@gmail.com>
2024-08-19 23:49:39 +0900

    Feat : 프로젝트 초기 설정

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | gugi',
+    default: 'gugi',
+  },
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
