9658a0d2c13fa551b5affb72babee3f2f6a2ab9d
choiyz <choirub@gmail.com>
2024-11-22 01:37:31 +0900

    Feat : 프로필 설정 step 2 자기소개글 작성

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 
 import ProfileStep1 from './components/ProfileStep1';
+import ProfileStep2 from './components/ProfileStep2';
 
 export default function ProfilePage() {
   const [step, setStep] = useState(1);
@@ -17,6 +18,13 @@ export default function ProfilePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       {step === 1 && <ProfileStep1 onNext={handleNextStep} />}
+      {step === 2 && (
+        <ProfileStep2 onNext={handleNextStep} onBack={handlePrevStep} />
+      )}
+
+      {/* {step === 3 && (
+        <ProfileStep3 onFinish={() => alert("프로필 설정 완료!")} onBack={handlePreviousStep} />
+      )}  */}
     </div>
   );
 }
