91c8095f698efde43088a77985e720cdd8735e04
choiyz <choirub@gmail.com>
2024-12-06 01:15:54 +0900

    Feat : 구단 바로가기 캐러셀

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,14 @@
 import Header from '@/components/Header';
 import Navigation from '@/components/Navigation';
 import Banner from './components/Banner';
+import Carousel from './components/Carousel';
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <Banner />
+      <Carousel />
       <Navigation />
     </>
   );
