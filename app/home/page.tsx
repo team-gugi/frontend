bee5121be32b14e7d598ac95ab5415da6c78c67e
choiyz <choirub@gmail.com>
2024-12-06 00:45:06 +0900

    Feat : 공통 컴포넌트 header, navigation

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,13 @@
-import Header from './components/Header';
+import Header from '@/components/Header';
+import Navigation from '@/components/Navigation';
+import Banner from './components/Banner';
 
-export default function Home() {
-  return <Header />;
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Navigation />
+    </>
+  );
 }
