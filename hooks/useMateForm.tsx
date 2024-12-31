c4f55a0732b4f9bfe8a49cf2538d2624c10dd3bd
choiyz <choirub@gmail.com>
2024-12-31 15:05:39 +0900

    Feat : useMateForm 수정

--- hooks/useMateForm.tsx
+++ hooks/useMateForm.tsx
@@ -1,32 +1,3 @@
-// import { mateFormState } from '@/recoil/mate/mateAtom';
-// import { useRecoilState } from 'recoil';
-
-// export function useMateForm() {
-//   const [formState, setFormState] = useRecoilState(mateFormState);
-
-//   const handleInputChange = (
-//     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-//   ) => {
-//     const { name, value } = e.target;
-//     setFormState((prev) => ({ ...prev, [name]: value }));
-//   };
-
-//   const handleOptionSelect = (option: string) => {
-//     setFormState((prev) => ({
-//       ...prev,
-//       options: prev.options.includes(option)
-//         ? prev.options.filter((o) => o !== option)
-//         : [...prev.options, option],
-//     }));
-//   };
-
-//   return {
-//     formState,
-//     handleInputChange,
-//     handleOptionSelect,
-//   };
-// }
-
 import { useState } from 'react';
 
 export const useMateForm = () => {
