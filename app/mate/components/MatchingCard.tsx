728e66eebbdc255e4aeac52e9f1e65ad275155c3
choiyz <choirub@gmail.com>
2025-01-02 06:03:40 +0900

    Feat : 직관 메이트 메인 화면 및 공통 컴포넌트

---
+++ app/mate/components/MatchingCard.tsx
@@ -0,0 +1,108 @@
+export default function MatchingCard() {
+  //   const optionLabels = [
+  //     '성별',
+  //     '연령',
+  //     '직관일자',
+  //     '응원팀',
+  //     '모집인원',
+  //     '구장',
+  //   ];
+
+  const optionLabels = [
+    '여자만',
+    '10대',
+    '08-21',
+    'KIA 타이거즈',
+    '4인',
+    '광주 기아 챔피언스필드',
+  ];
+  return (
+    <>
+      <div className="relative flex flex-col gap-20 pt-16 border-solid border-t-1 border-LightGray">
+        <div className="relative">
+          <span className="absolute -top-8 left-24 z-10 px-10 py-3 rounded-[10px] bg-MainColor text-White text-14 font-normal ">
+            D-4
+          </span>
+          <div className="flex flex-col px-24 py-20 bg-SemiWhite gap-13 rounded-[10px] min-h-234">
+            <div className="flex flex-row justify-between">
+              <span className="text-14 text-SemiBlack font-medium">
+                같이 직관 갈 두산 팬들 구합니다!
+              </span>
+              <span className="text-12 text-Gray font-medium">2일 전</span>
+            </div>
+
+            <div className="flex text-12 text-Gray font-normal leading-[140%] pb-13 border-solid border-b-[0.5px] border-Gray">
+              먹는 거 좋아해요. 많이 먹고 싶어요! mbti E인 분들 환영합니다
+              🐻먹는 거 좋아해요. 많이 먹고 싶어요! mbti E인 분들 환영합니다 🐻
+            </div>
+
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex flex-wrap gap-4">
+                {optionLabels.map((label, index) => (
+                  <div
+                    key={index}
+                    className="px-14 py-4 border-solid border-1 border-BlockColor bg-White text-SemiBlack text-12 font-medium whitespace-nowrap"
+                    style={{ borderRadius: '30px' }}
+                  >
+                    {label}
+                  </div>
+                ))}
+              </div>
+              <div className="flex flex-col items-center justify-center min-w-64 leading-[100%] min-h-38 px-10 py-5 rounded-[10px] border-solid border-1 border-MainColor bg-White">
+                <span className="text-11 text-MainColor font-medium">
+                  모집인원
+                </span>
+                <span className="text-11 text-MainColor font-medium">2/4</span>
+              </div>
+            </div>
+            <button className="flex px-85 py-10 items-center justify-center bg-MainColor text-White text-16 font-semibold rounded-xl">
+              매칭 신청하기
+            </button>
+          </div>
+        </div>
+
+        <div className="relative">
+          <span className="absolute -top-8 left-24 z-10 px-10 py-3 rounded-[10px] bg-MainColor text-White text-14 font-normal ">
+            D-4
+          </span>
+          <div className="flex flex-col px-24 py-20 bg-SemiWhite gap-13 rounded-[10px] min-h-234">
+            <div className="flex flex-row justify-between">
+              <span className="text-14 text-SemiBlack font-medium">
+                같이 직관 갈 두산 팬들 구합니다!
+              </span>
+              <span className="text-12 text-Gray font-medium">2일 전</span>
+            </div>
+
+            <div className="flex text-12 text-Gray font-normal leading-[140%] pb-13 border-solid border-b-[0.5px] border-Gray">
+              먹는 거 좋아해요. 많이 먹고 싶어요! mbti E인 분들 환영합니다
+              🐻먹는 거 좋아해요. 많이 먹고 싶어요! mbti E인 분들 환영합니다 🐻
+            </div>
+
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex flex-wrap gap-4">
+                {optionLabels.map((label, index) => (
+                  <div
+                    key={index}
+                    className="px-14 py-4 border-solid border-1 border-BlockColor bg-White text-SemiBlack text-12 font-medium whitespace-nowrap"
+                    style={{ borderRadius: '30px' }}
+                  >
+                    {label}
+                  </div>
+                ))}
+              </div>
+              <div className="flex flex-col items-center justify-center min-w-64 leading-[100%] min-h-38 px-10 py-5 rounded-[10px] border-solid border-1 border-MainColor bg-White">
+                <span className="text-11 text-MainColor font-medium">
+                  모집인원
+                </span>
+                <span className="text-11 text-MainColor font-medium">2/4</span>
+              </div>
+            </div>
+            <button className="flex px-85 py-10 items-center justify-center bg-MainColor text-White text-16 font-semibold rounded-xl">
+              매칭 신청하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
