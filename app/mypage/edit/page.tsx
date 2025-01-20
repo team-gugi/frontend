5ec88f8a7f9adf357fdf4c1e6822887333638c7d
choiyz <choirub@gmail.com>
2025-01-20 21:14:35 +0900

    Feat : 마이페이지 유저 프로필 수정

---
+++ app/mypage/edit/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header';
+import Navigation from '@/components/Navigation';
+import ProfileEditForm from '../components/ProfileEditForm';
+
+export default function MyPageEditPage() {
+  return (
+    <>
+      <Header />
+      <ProfileEditForm />
+      <Navigation />
+    </>
+  );
+}
