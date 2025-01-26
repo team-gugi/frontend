e9f65d9d8969465120dd520ca58cd80a8eec40b7
choiyz <choirub@gmail.com>
2025-01-26 13:20:16 +0900

    Feat : 회원 로그아웃

--- app/mypage/edit/page.tsx
+++ app/mypage/edit/page.tsx
@@ -1,12 +1,14 @@
 import Header from '@/components/Header';
 import Navigation from '@/components/Navigation';
 import ProfileEditForm from '../components/ProfileEditForm';
+import Account from '../components/Account';
 
 export default function MyPageEditPage() {
   return (
     <>
       <Header />
       <ProfileEditForm />
+      <Account />
       <Navigation />
     </>
   );
