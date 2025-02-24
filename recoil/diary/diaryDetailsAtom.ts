876a325beb5b087e3fe8c680e4922a1ae0bf7dc7
choiyz <choirub@gmail.com>
2025-02-25 00:51:06 +0900

    UI : 직관 일기 상세 페이지 Ticket UI 디자인 수정

--- recoil/diary/diaryDetailsAtom.ts
+++ recoil/diary/diaryDetailsAtom.ts
@@ -1,24 +1,3 @@
-// import { atom } from 'recoil';
-// // import { DiaryEntry } from './diaryAtom';
-
-// export interface IDiaryDetails {
-//   diaryId: string;
-//   gameDate: string;
-//   gameStadium: string;
-//   homeTeam: string;
-//   awayTeam: string;
-//   homeScore: number;
-//   awayScore: number;
-//   gameImg: string;
-//   content: string;
-//   gameResult: 'WIN' | 'LOSE' | 'DRAW';
-// }
-
-// export const diaryDetailsAtom = atom<IDiaryDetails | null>({
-//   key: 'diaryDetailsAtom',
-//   default: null,
-// });
-
 import { atom } from 'recoil';
 
 export interface IDiaryDetails {
