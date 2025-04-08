78a6781de9f79ebc0b6d3725323262a0507ae757
choiyz <choirub@gmail.com>
2025-04-08 12:54:50 +0900

    Fix : API url 수정

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -9,40 +9,52 @@ import { useRecoilState } from 'recoil';
 import { profileAtom } from '@/recoil/profileAtom';
 
 export default function ProfilePage() {
-  // const [profile, setProfile] = useRecoilState(profileAtom);
-  // const [step, setStep] = useState(1);
+  const [profile, setProfile] = useRecoilState(profileAtom);
+  const [step, setStep] = useState(1);
 
-  // const [registerToken, setRegisterToken] = useState<string | null>(null);
+  const [registerToken, setRegisterToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const token = new URLSearchParams(window.location.search).get(
+        'register_token',
+      );
+      setRegisterToken(token);
+    }
+  }, []);
+  // console.log(registerToken);
 
   // const searchParams = useSearchParams();
 
-  // // useEffect(() => {
-  // //   const token = searchParams.get('register_token');
-  // //   if (token) {
-  // //     setRegisterToken(token);
-  // //   }
-  // // }, [searchParams]);
+  // useEffect(() => {
+  //   const token = searchParams.get('register_token');
+  //   if (token) {
+  //     setRegisterToken(token);
+  //   }
+  //   console.log(token);
+  // }, [searchParams]);
 
   // useEffect(() => {
   //   const token = new URLSearchParams(window.location.search).get(
   //     'register_token',
   //   );
+  //   console.log(token);
   //   if (token) {
   //     setRegisterToken(token);
   //   }
   // }, []);
 
-  // const handleNextStep = () => {
-  //   setStep((prevStep) => prevStep + 1);
-  // };
+  const handleNextStep = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
 
-  // const handlePrevStep = () => {
-  //   setStep((prevStep) => Math.max(prevStep - 1, 1));
-  // };
+  const handlePrevStep = () => {
+    setStep((prevStep) => Math.max(prevStep - 1, 1));
+  };
 
   return (
     <>
-      {/* <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <div className="flex flex-col items-center justify-center min-h-screen p-4">
         <Suspense fallback={<div>Loading...</div>}>
           {step === 1 && <ProfileStep1 onNext={handleNextStep} />}
           {step === 2 && (
@@ -55,7 +67,69 @@ export default function ProfilePage() {
             />
           )}
         </Suspense>
-      </div> */}
+      </div>
     </>
   );
 }
+
+// 'use client';
+// import React, { Suspense, useEffect, useState } from 'react';
+
+// import ProfileStep1 from './components/ProfileStep1';
+// import ProfileStep2 from './components/ProfileStep2';
+// import ProfileStep3 from './components/ProfileStep3';
+// import { useSearchParams } from 'next/navigation';
+// import { useRecoilState } from 'recoil';
+// import { profileAtom } from '@/recoil/profileAtom';
+
+// export default function ProfilePage() {
+//   // const [profile, setProfile] = useRecoilState(profileAtom);
+//   // const [step, setStep] = useState(1);
+
+//   // const [registerToken, setRegisterToken] = useState<string | null>(null);
+
+//   // const searchParams = useSearchParams();
+
+//   // // useEffect(() => {
+//   // //   const token = searchParams.get('register_token');
+//   // //   if (token) {
+//   // //     setRegisterToken(token);
+//   // //   }
+//   // // }, [searchParams]);
+
+//   // useEffect(() => {
+//   //   const token = new URLSearchParams(window.location.search).get(
+//   //     'register_token',
+//   //   );
+//   //   if (token) {
+//   //     setRegisterToken(token);
+//   //   }
+//   // }, []);
+
+//   // const handleNextStep = () => {
+//   //   setStep((prevStep) => prevStep + 1);
+//   // };
+
+//   // const handlePrevStep = () => {
+//   //   setStep((prevStep) => Math.max(prevStep - 1, 1));
+//   // };
+
+//   return (
+//     <>
+//       {/* <div className="flex flex-col items-center justify-center min-h-screen p-4">
+//         <Suspense fallback={<div>Loading...</div>}>
+//           {step === 1 && <ProfileStep1 onNext={handleNextStep} />}
+//           {step === 2 && (
+//             <ProfileStep2 onNext={handleNextStep} onBack={handlePrevStep} />
+//           )}
+//           {step === 3 && (
+//             <ProfileStep3
+//               onBack={handlePrevStep}
+//               registerToken={registerToken || ''}
+//             />
+//           )}
+//         </Suspense>
+//       </div> */}
+//     </>
+//   );
+// }
--- lib/api/kakaoAuthApi.ts
+++ lib/api/kakaoAuthApi.ts
@@ -1,5 +1,6 @@
 export const redirectToKakao = () => {
   // const kakaoLoginUrl = 'http://52.79.47.101:8080/oauth2/authorization/kakao';
-  const kakaoLoginUrl = 'https://www.team-gugi.site/oauth2/authorization/kakao';
+  // const kakaoLoginUrl = 'https://www.team-gugi.site/oauth2/authorization/kakao';
+  const kakaoLoginUrl = 'https://api.team-gugi.site/oauth2/authorization/kakao'; //url 변경
   window.location.href = kakaoLoginUrl;
 };
