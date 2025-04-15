78609a8c5267030734cae49473455962cc823285
choiyz <choirub@gmail.com>
2025-04-15 16:10:48 +0900

    Feat : 직관 일기 작성,수정 성공시 모달 추가

--- app/diary/post/page.tsx
+++ app/diary/post/page.tsx
@@ -4,6 +4,10 @@ import Header from '@/components/Header';
 import Navigation from '@/components/Navigation';
 import PageTitle from '@/components/PageTitle';
 import DiaryForm from '../components/DiaryForm';
+
+import Image from 'next/image';
+
+import CheckIcon from '../../public/icons/Circlecheck.svg';
 import { useState } from 'react';
 
 import { IDiaryInfo, postDiaryApi } from '@/lib/api/createDiaryApi';
@@ -13,6 +17,7 @@ import { useRouter } from 'next/navigation';
 export default function DiaryPostPage() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태
 
   const router = useRouter();
 
@@ -24,7 +29,14 @@ export default function DiaryPostPage() {
       const response = await postDiaryApi(diaryInfo, gameImg || undefined);
       if (response.isSuccess == true) {
         console.log('일기가 성공적으로 등록되었습니다');
-        router.push('/diary');
+
+        // 모달 표시
+        setIsModalOpen(true);
+        setTimeout(() => {
+          setIsModalOpen(false); // 모달 닫기
+          router.push('/diary'); // 페이지 이동
+        }, 1500); // 1.5초 후 이동
+        // router.push('/diary');
       }
     } catch (error) {
       setError('일기 등록에 실패 했습니다. 다시 시도 해주세요');
@@ -43,6 +55,17 @@ export default function DiaryPostPage() {
         )} */}
         <Navigation />
       </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-SemiBlack bg-opacity-50">
+          <div className="flex flex-col items-center justify-center gap-12 bg-BlockColor px-30 py-45 rounded-xl shadow-lg">
+            <Image src={CheckIcon} alt="check icon" width={20} height={20} />
+            <p className="text-18 font-extralight text-SemiBlack">
+              <span className="text-MainColor font-bold">일기 작성</span>이
+              완료되었습니다.
+            </p>
+          </div>
+        </div>
+      )}
     </>
   );
 }
