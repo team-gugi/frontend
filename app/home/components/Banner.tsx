c2acf9d2d359b969b5c8162205142d498daced73
choiyz <choirub@gmail.com>
2025-04-29 14:25:24 +0900

    UI : 메인 배너 이미지 변경

--- app/home/components/Banner.tsx
+++ app/home/components/Banner.tsx
@@ -3,12 +3,20 @@ import Text from '../components/BannerText';
 import Link from 'next/link';
 
 import ArrowIcon from '../../public/icons/Arrow.svg';
+import WhiteArrowIcon from '../../public/icons/White_bold_arrow.svg';
+import BannerImg1 from '../../public/icons/mainbanner.jpeg';
+import BannerImg2 from '../../public/icons/banner_main.jpeg';
 
 export default function Banner() {
   return (
     <div className="py-10 px-24">
-      <section className="h-190 rounded-lg mb-14 pt-33 pr-11 pb-11 pl-23 bg-SemiWhite">
-        <h2 className="text-24 font-bold text-SemiBlack mb-8">
+      <section
+        className="h-190 rounded-lg mb-14 pt-33 pr-11 pb-11 pl-23 bg-SemiWhite bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${BannerImg1.src})`, // 이미지 경로를 동적으로 설정
+        }}
+      >
+        <h2 className="text-24 font-extrabold text-White mb-10">
           야구가 처음이세요?
         </h2>
         <Text text="어렵지 않아요," />
@@ -16,15 +24,17 @@ export default function Banner() {
         {/* <div className=" flex flex-row justify-end mt-39 overflow-hidden px-14 py-8 gap-8"> */}
         <Link
           href="/chatbot"
-          className=" flex flex-row justify-end mt-39 overflow-hidden px-14 py-8 gap-8"
+          className="flex flex-row justify-start mt-39 overflow-hidden py-8 gap-8"
+          // className="flex flex-row justify-end mt-39 overflow-hidden px-14 py-8 gap-8"
         >
           {' '}
           {/* /chatbot 페이지로 이동하는 링크 추가 */}
-          <p className="text-14 text-semiBlack font-light cursor-pointer">
+          <p className="text-14 text-White  font-normal cursor-pointer">
+            {/* <p className="text-14 text-White  font-light cursor-pointer"> */}
             야구 룰 배워보기
           </p>
           <Image
-            src={ArrowIcon}
+            src={WhiteArrowIcon}
             alt="야구 룰 페이지 바로가기 버튼"
             width={22}
             height={8}
