c724beaaa293025a374d938d116a095a2451b684
choiyz <choirub@gmail.com>
2025-04-29 14:25:24 +0900

    UI : 메인 배너 이미지 변경

--- app/home/components/BannerText.tsx
+++ app/home/components/BannerText.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BannerText({ text }: { text: string }) {
   return (
-    <p className="text-16 font-normal text-SemiBlack mb-2 leading-[120%] tracking-[-4%]">
+    <p className="text-16 font-normal text-White  mb-2 leading-[120%] tracking-[-4%]">
       {text}
     </p>
   );
