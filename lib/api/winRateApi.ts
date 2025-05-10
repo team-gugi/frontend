3f55a42286a99d68248f6f4dd0c3c1bf7e59fe19
choiyz <choirub@gmail.com>
2025-05-10 22:42:39 +0900

    Fix : getWinRate 예외처리 수정

--- lib/api/winRateApi.ts
+++ lib/api/winRateApi.ts
@@ -1,7 +1,4 @@
-import { useRouter } from 'next/navigation';
-
 export const getWinRate = async () => {
-  const router = useRouter();
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/diary/win-rate`,
     { credentials: 'include' },
@@ -9,10 +6,6 @@ export const getWinRate = async () => {
 
   if (!response.ok) {
     throw new Error('승률 정보를 불러오는데 실패했습니다');
-  }
-
-  if (response.status === 404 || response.status === 401) {
-    router.push('/login');
   }
 
   const data = await response.json();
